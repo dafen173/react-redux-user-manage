d8b955f4a35d3ce35f22662c50b2fb427fafcef0
Alex <dafen@ukr.net>
2022-04-08 01:24:23 +0300

    block delete full group is done

--- client/src/component/DeleteGroup.js
+++ client/src/component/DeleteGroup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 //import {removeUser} from '../action/removeUser'
 import { removeGroup } from '../action/removeGroup'
 
@@ -7,10 +7,32 @@ function DeleteGroup (props) {
   const dispatch = useDispatch()
   const deleteId = props.group.id
 
+  const userSelector = useSelector(state => state.users.items)
+
   function deleteHandler () {
     console.log(deleteId)
-    dispatch(removeGroup(deleteId))
+    console.log(props.group.groupname)
+    //dispatch(removeGroup(deleteId))
+
+    let groupIsEmpty = true
+    for (let i = 0; i < userSelector.length; i++) { 
+      for (let key in userSelector[i]) { 
+        if (userSelector[i][key] === props.group.groupname) { 
+          //console.log("key is:" + key + " index is:" + i); 
+          console.log('yessssssssssssss')
+          alert('Cannot be deleted! This group already has users and cannot be deleted!')
+          groupIsEmpty = false
+        } 
+      } 
+    } 
+    if (groupIsEmpty) { 
+      console.log("grop is empty, can be deleted!!!")
+      dispatch(removeGroup(deleteId))
+    }
+    
   }
+
+
   return (  
     <React.Fragment>
       <button className="rm" onClick={deleteHandler}>
